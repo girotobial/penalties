5cd25ad1ba36910a056fe0bdbd8c1096d5995386
Alex Robinson <girotobial@gmail.com>
2023-03-26 12:42:29 +0100

    ♿️ add tooltips to car selector

--- src/lib/Car.svelte
+++ src/lib/Car.svelte
@@ -13,8 +13,12 @@
 <div class="mb-3">
   <label class="text-secondary" for="carSettings">Car {id}</label>
   <div class="mb-3" id="carSettings">
-    <TickBox bind:value={contact} label="Contact" />
-    <Slider bind:value={placesLost} name="Places Lost" {min} {max} />
+    <div title="Deducts 1 point for contact">
+      <TickBox bind:value={contact} label="Contact" />
+    </div>
+    <div title="0.5 points per place lost">
+      <Slider bind:value={placesLost} name="Places Lost" {min} {max} />
+    </div>
   </div>
 </div>
 
