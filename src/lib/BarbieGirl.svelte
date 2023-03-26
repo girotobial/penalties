64bccb59fdc416baa8f58234edbbae731871668e
Alex Robinson <girotobial@gmail.com>
2023-03-26 15:13:35 +0100

    autoplay barbiegirl

--- src/lib/BarbieGirl.svelte
+++ src/lib/BarbieGirl.svelte
@@ -3,7 +3,7 @@
     class="embed-responsive embed-responsive-4by3"
     width="560"
     height="315"
-    src="https://www.youtube.com/embed/ZyhrYis509A?start=14"
+    src="https://www.youtube.com/embed/ZyhrYis509A?start=14&autoplay=1"
     title="YouTube video player"
     frameborder="0"
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
