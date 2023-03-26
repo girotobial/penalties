95b37fd527a7d11c4a30531f30e1fa19569e0c68
Alex Robinson <girotobial@gmail.com>
2023-03-26 16:28:54 +0100

    💄 update iframe design

--- src/lib/BarbieGirl.svelte
+++ src/lib/BarbieGirl.svelte
@@ -1,22 +1,18 @@
-<div class="embed-responsive embed-responsive-4by3">
-  <iframe
-    class="embed-responsive-item"
-    src="https://www.youtube.com/embed/ZyhrYis509A?start=14&autoplay=1"
-    title="YouTube video player"
-    frameborder="0"
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-    allowfullscreen
-  />
+<div class="container">
+  <div class="embed-responsive">
+    <iframe
+      src="https://www.youtube.com/embed/ZyhrYis509A?start=14&autoplay=1&rel=0"
+      title="YouTube video player"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen
+    />
+  </div>
   <p class="text-danger">Timmy that you?</p>
 </div>
 
 <style>
-  div {
+  .container {
     padding: 1rem 1.5rem;
     text-align: center;
-  }
-
-  iframe {
-    max-width: 560px;
   }
 </style>
