5d663cc6933eda40e836cd0b710dc7481bde8cc8
Alex Robinson <girotobial@gmail.com>
2023-03-26 20:03:46 +0100

    ♻️ seperate youtube into it's own component

--- src/lib/BarbieGirl.svelte
+++ src/lib/BarbieGirl.svelte
@@ -1,24 +1,14 @@
+<script lang="ts">
+  import Youtube from "./Youtube.svelte";
+</script>
+
 <div class="centered">
-  <div class="container-sm ratio ratio-16x9 mb-3 embed-responsive youtube">
-    <iframe
-      class="embed-responsive-item"
-      src="https://www.youtube.com/embed/ZyhrYis509A?start=14&autoplay=1&rel=0"
-      title="YouTube video player"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      allowfullscreen
-    />
-  </div>
+  <Youtube videoId="ZyhrYis509A" start={14} autoplay />
   <p class="text-danger">Timmy that you?</p>
 </div>
 
 <style>
   .centered {
     text-align: center;
-  }
-  .youtube {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    /* max-width: 516px; */
   }
 </style>
---
+++ src/lib/Youtube.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  export let videoId: string;
+  export let start = -1;
+  export let autoplay = false;
+
+  function getStartUrl(s: number): string {
+    if (s >= 0) {
+      return "start=" + s;
+    }
+    return "";
+  }
+
+  $: autoplayVal = Number(autoplay);
+  $: startUrl = getStartUrl(start);
+  $: url = `https://www.youtube.com/embed/${videoId}?${startUrl}&autoplay=${autoplayVal}&rel=0`;
+</script>
+
+<div class="container-sm ratio ratio-16x9 mb-3 embed-responsive youtube">
+  <iframe
+    class="embed-responsive-item"
+    src={url}
+    title="YouTube video player"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    allowfullscreen
+  />
+</div>
+
+<style>
+  .youtube {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
